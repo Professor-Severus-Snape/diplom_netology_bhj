276eae502f7b3bb0176683b1792f7d56c61ba03d
Julia Fokanova <oncologmma@mail.ru>
2024-04-08 00:17:31 +0300

    6. авторизация

--- public/js/ui/forms/LoginForm.js
+++ public/js/ui/forms/LoginForm.js
@@ -10,6 +10,16 @@ class LoginForm extends AsyncForm {
    * закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+    const callback = (err, response) => {
+      // Варианты response:
+      // response = {success: true, user: {name: "demo", email: "demo@demo", password: "demo", id: "1"}}
+      // response = {success: false, error: "Пользователь c email demo@demo и паролем 123 не найден"}
+      if (response.success && response.user) {
+        this.element.reset();
+        App.setState("user-logged");
+        App.getModal("login").close();
+      }
+    }
+    User.login(data, callback);
   }
 }
